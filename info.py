c0655136b289fda8f4d8c8c7accbc3251bbd0a63
rajgu764-ui <rajgu764@gmail.com>
2026-04-17 22:23:04 +0600

    Update info.py
    Signed-off-by: rajgu764-ui <rajgu764@gmail.com>

--- info.py
+++ info.py
@@ -63,7 +63,7 @@ IS_SHORTLINK = is_enabled(environ.get('IS_SHORTLINK', "True"), True)
 # Verification Config
 VERIFY_EXPIRE = int(environ.get('VERIFY_EXPIRE', 60)) # In Minutes/Hours based on logic
 SHORTLINK_URL = environ.get('SHORTLINK_URL', 'mdiskshortner.link')
-SHORTLINK_API = environ.get('SHORTLINK_API', '96a3c0e8ae1b1abd429906762e38a40d3f2ec56c')
+SHORTLINK_API = environ.get('SHORTLINK_API', '52dc53b84036e2b3d10179e70bf4606b4a25ff76')
 
 # Second Verification Config
 SHORTLINK_WEBSITE2 = environ.get("SHORTENER_WEBSITE2", "mdiskshortner.link")
