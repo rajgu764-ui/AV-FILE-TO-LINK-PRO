09e039f0cd8d90c3a60123452d6bdd326720c323
rajgu764-ui <rajgu764@gmail.com>
2026-04-19 00:24:44 +0600

    Update Script.py
    Signed-off-by: rajgu764-ui <rajgu764@gmail.com>

--- Script.py
+++ Script.py
@@ -164,11 +164,11 @@ class script(object):
     ABOUT_TXT = """<b>╔══❰ {} ❱═════❍
 ║╭━━━━━━━━━━━━━━━━━━➣
 ║┣⪼🤖 ᴍʏ ɴᴀᴍᴇ : {}
-║┣⪼👦 ᴅᴇᴠᴇʟᴏᴘᴇʀ : <a href='https://t.me/AV_OWNER_BOT'>ᴀᴠ ᴄʜᴀᴛ ᴏᴡɴᴇʀ</a>
-║┣⪼❣️ ᴜᴘᴅᴀᴛᴇ : <a href=https://t.me/AV_BOTz_UPDATE>ᴀᴠ ʙᴏᴛᴢ</a>
+║┣⪼👦 ᴅᴇᴠᴇʟᴏᴘᴇʀ : <a href='https://t.me/ratul17779'>Ratul</a>
+║┣⪼❣️ ᴜᴘᴅᴀᴛᴇ : <a href=https://t.me/InfinityCloudUpdates>Infinity Cloud Updates</a>
 ║┣⪼⏲️ ʙᴏᴛ ᴜᴘᴛɪᴍᴇ :- {}
-║┣⪼📡 ʜᴏsᴛᴇᴅ ᴏɴ : ᴋᴏʏᴇʙ 
-║┣⪼🗣️ ʟᴀɴɢᴜᴀɢᴇ : ᴘʏᴛʜᴏɴ 
+║┣⪼📡 ʜᴏsᴛᴇᴅ ᴏɴ : YPS
+║┣⪼🗣️ ʟᴀɴɢᴜᴀɢᴇ : ᴘʏᴛʜᴏɴ
 ║┣⪼📚 ʟɪʙʀᴀʀʏ : ᴘʏʀᴏɢʀᴀᴍ
 ║┣⪼🗒️ ᴠᴇʀsɪᴏɴ : {} [sᴛᴀʙʟᴇ]
 ║╰━━━━━━━━━━━━━━━➣
@@ -248,7 +248,7 @@ class script(object):
 💸 <b>ᴜᴘɪ ɪᴅ:</b> <code>.......</code> (ᴛᴀᴘ ᴛᴏ ᴄᴏᴘʏ)
 
 📸 <i>sᴇɴᴅ ᴘᴀʏᴍᴇɴᴛ sᴄʀᴇᴇɴsʜᴏᴛ ᴛᴏ ᴀᴅᴍɪɴ ꜰᴏʀ ᴀᴄᴛɪᴠᴀᴛɪᴏɴ.</i>
-🏷️ <a href='https://t.me/AV_PREMIUM_PROOF'>ᴄʜᴇᴄᴋ ᴘʀᴇᴍɪᴜᴍ ᴘʀᴏᴏꜰs</a>"""
+🏷️ <a href='https://t.me/ratul17779'>🧾ᴘʀᴇᴍɪᴜᴍ ᴘʀᴏᴏꜰ</a>"""
     
     PREMIUM_TEXT = """<b>👑 <u>ᴘʀᴇᴍɪᴜᴍ sᴛᴀᴛᴜs</u></b>
 
