d3550bdaef35344ce7ac4ddefdeccc773f699d52
rajgu764-ui <rajgu764@gmail.com>
2026-04-21 01:11:24 +0600

    Update info.py
    Signed-off-by: rajgu764-ui <rajgu764@gmail.com>

--- info.py
+++ info.py
@@ -102,7 +102,7 @@ FILE_CAPTION = environ.get('FILE_CAPTION', script.CAPTION)
 # =========================================================
 WORKERS = int(getenv('WORKERS', '4'))
 MULTI_CLIENT = False
-name = str(environ.get('name', 'avbotz'))
+name = str(environ.get('name', 'infinity-linker'))
 
 # Heroku & Port Config
 if 'DYNO' in environ:
