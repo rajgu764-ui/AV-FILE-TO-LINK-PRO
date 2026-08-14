896f0d035009178c2ad10e1c14070c288ff648ff
rajgu764-ui <rajgu764@gmail.com>
2026-08-14 09:16:25 +0600

    Update environment variables for verification and limits
    Signed-off-by: rajgu764-ui <rajgu764@gmail.com>

--- info.py
+++ info.py
@@ -57,17 +57,17 @@ TUTORIAL_LINK_2 = environ.get('TUTORIAL_LINK_2', 'https://t.me/2')
 # ðŸ” VERIFICATION & SHORTENER
 # =========================================================
 IS_VERIFY = is_enabled(environ.get("IS_VERIFY", "True"), True)
-IS_SECOND_VERIFY = is_enabled(environ.get("IS_SECOND_VERIFY", "False"), False)
+IS_SECOND_VERIFY = is_enabled(environ.get("IS_SECOND_VERIFY", "True"), True)
 IS_SHORTLINK = is_enabled(environ.get('IS_SHORTLINK', "True"), True)
 
 # Verification Config
-VERIFY_EXPIRE = int(environ.get('VERIFY_EXPIRE', 60)) # In Minutes/Hours based on logic
-SHORTLINK_URL = environ.get('SHORTLINK_URL', 'yourdestinationlink.com')
-SHORTLINK_API = environ.get('SHORTLINK_API', 'fe5e689e49f014fbfc697849512a604564006c39')
+VERIFY_EXPIRE = int(environ.get('VERIFY_EXPIRE', 1)) # In Minutes/Hours based on logic
+SHORTLINK_URL = environ.get('SHORTLINK_URL', 'cuty.io')
+SHORTLINK_API = environ.get('SHORTLINK_API', 'fe5e689e49f014fbfc6978495')
 
 # Second Verification Config
-SHORTLINK_WEBSITE2 = environ.get("SHORTENER_WEBSITE2", "yourdestinationlink.com")
-SHORTLINK_API2 = environ.get("SHORTENER_API2", "fe5e689e49f014fbfc697849512a604564006c39")
+SHORTLINK_WEBSITE2 = environ.get("SHORTENER_WEBSITE2", "gplinks.com")
+SHORTLINK_API2 = environ.get("SHORTENER_API2", "3cd0d6f8f5abfdb64341f77a88d3fb6b1f6851b7")
 
 # =========================================================
 # âš™ï¸ SETTINGS & LIMITS
@@ -77,22 +77,22 @@ ENABLE_LIMIT = is_enabled(environ.get("ENABLE_LIMIT", "True"), True)
 MAINTENANCE_MODE = is_enabled(environ.get("MAINTENANCE_MODE", "False"), False)
 
 # Time & Rate Limits
-TIMEZONE = environ.get("TIMEZONE", "Asia/Kolkata")
+TIMEZONE = environ.get("TIMEZONE", "Asia/Dhaka")
 PING_INTERVAL = int(environ.get("PING_INTERVAL", "1200"))
 SLEEP_THRESHOLD = int(getenv('SLEEP_THRESHOLD', '60'))
 RATE_LIMIT_TIMEOUT = int(environ.get("RATE_LIMIT_TIMEOUT", "600"))
 
 # File Limits
-MAX_FILES = int(environ.get("MAX_FILES", "5"))
-BATCH_LIMIT = int(environ.get('BATCH_LIMIT', 60))
+MAX_FILES = int(environ.get("MAX_FILES", "10"))
+BATCH_LIMIT = int(environ.get('BATCH_LIMIT', 5))
 
 # =========================================================
 # ðŸ–¼ï¸ MEDIA & CAPTIONS
 # =========================================================
 QR_CODE = environ.get('QR_CODE', 'https://graph.org/file/6afb4093d5ec5c4176979.jpg')
 VERIFY_IMG = environ.get("VERIFY_IMG", "https://graph.org/file/1669ab9af68eaa62c3ca4.jpg")
-AUTH_PICS = environ.get('AUTH_PICS', 'https://envs.sh/AwV.jpg')
-PICS = environ.get('PICS', 'https://ibb.co/VpTJNNCN')
+AUTH_PICS = environ.get('AUTH_PICS', 'https://i.ibb.co.com/bjNL5Csj/file-00000000279871fa93dc17179e399163.png')
+PICS = environ.get('PICS', 'https://i.ibb.co.com/bjNL5Csj/file-00000000279871fa93dc17179e399163.png')
 FILE_PIC = environ.get('FILE_PIC', 'https://i.ibb.co/bj4My0bW/photo-2025-07-21-02-15-21-7529360175656861700.jpg')
 
 FILE_CAPTION = environ.get('FILE_CAPTION', script.CAPTION)
